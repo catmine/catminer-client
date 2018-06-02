0a75181bb8b366cfabf7acdfbb54accf5b065871
Sukrit Sunama <sunama.sukrit@gmail.com>
2018-06-02 18:09:37 +0700

    fix miners path

--- lib/models/miners.rb
+++ lib/models/miners.rb
@@ -19,36 +19,36 @@ module CatminerClient
       run_locally do
         execute 'sudo apt-get install -y build-essential autoconf automake libtool pkg-config libcurl4-gnutls-dev uthash-dev libncursesw5-dev libjansson-dev autotools-dev gcc-5 g++-5'
 
-        test "git clone https://github.com/tpruvot/ccminer #{Dir.pwd}/miners/ccminer"
-        execute "cd #{Dir.pwd}/miners/ccminer && git pull"
+        test "git clone https://github.com/tpruvot/ccminer #{Dir.pwd}/vendor/miners/ccminer"
+        execute "cd #{Dir.pwd}/vendor/miners/ccminer && git pull"
 
-        execute "cd #{Dir.pwd}/miners/ccminer && ./autogen.sh"
-        execute "cd #{Dir.pwd}/miners/ccminer && ./configure"
-        execute "cd #{Dir.pwd}/miners/ccminer && make"
+        execute "cd #{Dir.pwd}/vendor/miners/ccminer && ./autogen.sh"
+        execute "cd #{Dir.pwd}/vendor/miners/ccminer && ./configure"
+        execute "cd #{Dir.pwd}/vendor/miners/ccminer && make"
       end
     end
 
     def claymore
       run_locally do
-        execute "wget https://github.com/nanopool/Claymore-Dual-Miner/releases/download/v10.0/Claymore.s.Dual.Ethereum.Decred_Siacoin_Lbry_Pascal.AMD.NVIDIA.GPU.Miner.v10.0.-.LINUX.tar.gz -P #{Dir.pwd}/miners"
+        execute "wget https://github.com/nanopool/Claymore-Dual-Miner/releases/download/v10.0/Claymore.s.Dual.Ethereum.Decred_Siacoin_Lbry_Pascal.AMD.NVIDIA.GPU.Miner.v10.0.-.LINUX.tar.gz -P #{Dir.pwd}/vendor/miners"
         test "mkdir #{Dir.pwd}/vendor/miners/claymore"
-        execute "tar -xvzf #{Dir.pwd}/miners/Claymore.s.Dual.Ethereum.Decred_Siacoin_Lbry_Pascal.AMD.NVIDIA.GPU.Miner.v10.0.-.LINUX.tar.gz -C #{Dir.pwd}/vendor/miners/claymore"
+        execute "tar -xvzf #{Dir.pwd}/vendor/miners/Claymore.s.Dual.Ethereum.Decred_Siacoin_Lbry_Pascal.AMD.NVIDIA.GPU.Miner.v10.0.-.LINUX.tar.gz -C #{Dir.pwd}/vendor/miners/claymore"
       end
     end
 
     def ethminer
       run_locally do
-        execute "wget https://github.com/ethereum-mining/ethminer/releases/download/v0.13.0rc7/ethminer-0.13.0rc7-Linux.tar.gz -P #{Dir.pwd}/miners"
+        execute "wget https://github.com/ethereum-mining/ethminer/releases/download/v0.13.0rc7/ethminer-0.13.0rc7-Linux.tar.gz -P #{Dir.pwd}/vendor/miners"
         test "mkdir #{Dir.pwd}/vendor/miners/ethminer"
-        execute "tar -xvzf #{Dir.pwd}/miners/ethminer-0.13.0rc7-Linux.tar.gz -C #{Dir.pwd}/vendor/miners/ethminer"
+        execute "tar -xvzf #{Dir.pwd}/vendor/miners/ethminer-0.13.0rc7-Linux.tar.gz -C #{Dir.pwd}/vendor/miners/ethminer"
       end
     end
 
     def ewbf_miner
       run_locally do
-        execute "wget https://github.com/nanopool/ewbf-miner/releases/download/v0.3.4b/Zec.miner.0.3.4b.Linux.Bin.tar.gz -P #{Dir.pwd}/miners"
+        execute "wget https://github.com/nanopool/ewbf-miner/releases/download/v0.3.4b/Zec.miner.0.3.4b.Linux.Bin.tar.gz -P #{Dir.pwd}/vendor/miners"
         test "mkdir #{Dir.pwd}/vendor/miners/ewbf-miner"
-        execute "tar -xvzf #{Dir.pwd}/miners/Zec.miner.0.3.4b.Linux.Bin.tar.gz -C #{Dir.pwd}/vendor/miners/ewbf-miner"
+        execute "tar -xvzf #{Dir.pwd}/vendor/miners/Zec.miner.0.3.4b.Linux.Bin.tar.gz -C #{Dir.pwd}/vendor/miners/ewbf-miner"
       end
     end
   end
